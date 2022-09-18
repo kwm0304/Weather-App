9e25274088898e8f6c38dd6cc2918910d1978354
Kenan <mckenzie.kenan@yahoo.com>
2022-09-17 21:07:38 -0400

    plan for script

---
+++ script.js
@@ -0,0 +1,14 @@
+//1.function to initialize page
+//2.Function scoped variables
+//3. fetchWeather(city)
+    //3a.function scoped variables
+    //3b.fetch(url)->response->data->catch error
+//4.showWeather(response)
+    //4a.function scoped variables
+        //currentEl's, 
+    //4b.link 4a to html for current city
+        //currentEl->response
+    //4c.variables for 5-day forecast
+    //4d.link 4c to html for corresponding days (iterate?) or list each
+        //day#El->response
+//link search bar to search button  
